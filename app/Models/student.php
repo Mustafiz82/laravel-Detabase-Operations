bbfb58b46f411dccc48408aedfb88405a406c29e
Mustafiz82 <mustafiz8260@gmail.com>
2024-12-03 23:56:28 +0600

    Database Mutation

--- app/Models/student.php
+++ app/Models/student.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class student extends Model
 {
     protected $primaryKey = 'Id';
+
+    //Ansestor example
+    function getNameAttribute($a){
+        return ucfirst($a)." Rahman";
+    }
+
+    //Mutator example
+    function setNameAttribute ($val){
+        // $this->attributes['Name'] = ucfirst($value);
+        $this->attributes['Name'] = ucfirst($val);
+    }
 }
